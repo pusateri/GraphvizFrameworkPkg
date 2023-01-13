b9acfe5e364e896c5538cfd14306a9677b5a9e34
Tom Pusateri <pusateri@togetherpie.com>
2023-01-13 09:58:42 -0500

    First attempt of turning framework into swift package

---
+++ README.md
@@ -0,0 +1,20 @@
+# GraphvizFrameworkPkg
+
+The graphviz library components built into an XCFramework and wrapped up in a Swift Package
+
+Based on graphviz 7.0.6
+
+Includes:
+    * cgraph
+    * cdt
+    * common
+    * gvc
+    * label
+    * pack
+    * pathplan
+    * xdot
+    
+Binary Frameworks for the following operating systems:
+    * iOS
+    * macOS
+    * iOS Simulator
---
+++ Sources/GraphvizFramework.xcframework/ios-arm64_x86_64-simulator/graphvizFramework.framework/Headers/graphvizFramework.h
@@ -0,0 +1,18 @@
+//
+//  graphvizFramework.h
+//  graphvizFramework
+//
+//  Created by Tom Pusateri on 1/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for graphvizFramework.
+FOUNDATION_EXPORT double graphvizFrameworkVersionNumber;
+
+//! Project version string for graphvizFramework.
+FOUNDATION_EXPORT const unsigned char graphvizFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <graphvizFramework/PublicHeader.h>
+
+
