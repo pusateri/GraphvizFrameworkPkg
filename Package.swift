48bdc24c43943d2208564fcc904c9eb60abb42d4
Tom Pusateri <pusateri@togetherpie.com>
2023-01-13 12:55:07 -0500

    Add new version with small g.

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "GraphvizFramework",
-            targets: ["GraphvizFramework"]),
+            name: "graphvizFramework",
+            targets: ["graphvizFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "GraphvizFramework",
-            path: "./Sources/GraphvizFramework.xcframework"),
+            name: "graphvizFramework",
+            path: "./Sources/graphvizFramework.xcframework"),
     ]
 )
