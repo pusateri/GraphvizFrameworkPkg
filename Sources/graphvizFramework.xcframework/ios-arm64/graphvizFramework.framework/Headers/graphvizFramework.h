262312da05c11e2c4b391576981f320d1a3802ef
Tom Pusateri <pusateri@togetherpie.com>
2023-01-13 14:49:03 -0500

    Add new framework with public header files.

--- Sources/graphvizFramework.xcframework/ios-arm64/graphvizFramework.framework/Headers/graphvizFramework.h
+++ Sources/graphvizFramework.xcframework/ios-arm64/graphvizFramework.framework/Headers/graphvizFramework.h
@@ -13,6 +13,29 @@ FOUNDATION_EXPORT double graphvizFrameworkVersionNumber;
 //! Project version string for graphvizFramework.
 FOUNDATION_EXPORT const unsigned char graphvizFrameworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <graphvizFramework/PublicHeader.h>
-
+#import <graphvizFramework/arith.h>
+#import <graphvizFramework/cdt.h>
+#import <graphvizFramework/cghdr.h>
+#import <graphvizFramework/cgraph.h>
+#import <graphvizFramework/color.h>
+#import <graphvizFramework/geom.h>
+#import <graphvizFramework/graphviz_version.h>
+#import <graphvizFramework/gvc.h>
+#import <graphvizFramework/gvcext.h>
+#import <graphvizFramework/gvcjob.h>
+#import <graphvizFramework/gvcommon.h>
+#import <graphvizFramework/gvconfig.h>
+#import <graphvizFramework/gvplugin.h>
+#import <graphvizFramework/gvplugin_device.h>
+#import <graphvizFramework/gvplugin_layout.h>
+#import <graphvizFramework/gvplugin_loadimage.h>
+#import <graphvizFramework/gvplugin_render.h>
+#import <graphvizFramework/gvplugin_textlayout.h>
+#import <graphvizFramework/pack.h>
+#import <graphvizFramework/pathgeom.h>
+#import <graphvizFramework/pathplan.h>
+#import <graphvizFramework/textspan.h>
+#import <graphvizFramework/types.h>
+#import <graphvizFramework/usershape.h>
+#import <graphvizFramework/xdot.h>
 
